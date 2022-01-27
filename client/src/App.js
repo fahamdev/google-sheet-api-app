5bae2ceef4afd849a21dee799a480bafd37253ce
fahamdev <faham.dev@gmail.com>
2022-01-27 18:32:52 +0600

    add semantic UI

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import UserEntryForm from './components/UserEntryForm';
+import UserList from './components/UserList';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Google Sheet APP
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <body>
+        <div className="ui container Top-margin">
+          <UserEntryForm/>
+          <UserList/>
+        </div>
+      </body>
     </div>
   );
 }
---
+++ client/src/components/UserEntryForm.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const UserEntryForm = () => {
+  return <div>User Entry Form</div>;
+};
+
+export default UserEntryForm;
